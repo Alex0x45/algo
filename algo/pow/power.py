58816dcfa468ff076983f69872dc5746ed987b0b
Alex0x45 <Alex0x45@users.noreply.github.com>
2021-03-07 20:36:33 -0500

    Added tail recusive implementation for power

--- algo/pow/power.py
+++ algo/pow/power.py
@@ -1,11 +1,28 @@
 """Integer powers"""
 
 
-def pow(b, n):
+def recpow(b, n):
 
     if n == 0:
         return 1
     elif n % 2 == 0:
-        return pow(b * b, n // 2)
+        return recpow(b * b, n // 2)
     else:
-        return pow(b * b, (n - 1) // 2) * b
+        return recpow(b * b, (n - 1) // 2) * b
+
+
+def pow(b, n):
+
+    def accpow(b, n, acc):
+        
+        if n == 0:
+            return acc
+        elif n % 2 == 0:
+            return accpow(b * b, n // 2, acc)
+        else:
+            return accpow(b * b, (n - 1) // 2, acc * b)
+
+    if n == 0:
+        return 1
+    else:
+        return accpow(b, n, 1)
