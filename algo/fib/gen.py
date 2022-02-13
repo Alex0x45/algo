28a0530174133a5deacfb942612c3e1b0a053b71
Alex0x45 <Alex0x45@users.noreply.github.com>
2022-02-13 16:51:28 -0500

    added recfib

--- algo/fib/gen.py
+++ algo/fib/gen.py
@@ -57,3 +57,29 @@ def fastfib(n):
     m = n // 2
     (f2m, f2m1) = f2n_and_f2n1(m)
     return f2m if n % 2 == 0 else f2m1
+
+
+def recfib(n):
+
+    def f2n_and_f2n1(n, fk, fk1):
+
+        def fn_to_f2n(fn, fn1):
+            f2n = fn * (2 * fn1 - fn)
+            f2n1 = fn1 * fn1 + fn * fn
+            return (f2n, f2n1)
+
+        if n == 1:
+            return (fk, fk1)
+        else:
+            (f2k, f2k1) = fn_to_f2n(fk, fk1)
+            (fn, fn1) = (f2k, f2k1) if n % 2 == 0 else (f2k1, f2k + f2k1)
+            return f2n_and_f2n1(n // 2, fn, fn1)
+
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    m = n // 2
+    (f2m, f2m1) = f2n_and_f2n1(m, 1, 2)
+    return f2m if n % 2 == 0 else f2m1
